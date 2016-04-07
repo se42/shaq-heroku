f31f90bc5eed87c4d6de9dd16ccc9b4567cc7336
Scott Edwards <scott.edwards3@gmail.com>
2016-04-07 15:16:17 -0400

    add check_signed_URL test

--- qcforms/static/qcforms/qcforms_tests.js
+++ qcforms/static/qcforms/qcforms_tests.js
@@ -1,5 +1,5 @@
-QUnit.test("Test 403 response from private resource", function(assert) {
-	var original_url = document.getElementById("test-preview").src;
+QUnit.test("Unsigned URL returns HTTP status 403", function(assert) {
+	var original_url = document.getElementById("test-unsigned").src;
 	var done = assert.async();
 
 	var xhr = new XMLHttpRequest();
@@ -14,7 +14,7 @@ QUnit.test("Test 403 response from private resource", function(assert) {
 });
 
 
-QUnit.test("Test URL updates", function(assert) {
+QUnit.test("Target src is updated to properly signed URL", function(assert) {
 	var reference_url = document.getElementById("test-reference").src;
 	var original_url = document.getElementById("test-preview").src;
 	var done = assert.async();
@@ -23,16 +23,31 @@ QUnit.test("Test URL updates", function(assert) {
 
 	function check_urls() {
 		var updated_url = document.getElementById("test-preview").src;
-		console.log("Reference URL: " + reference_url);
-		console.log("Original URL: " + original_url);
-		console.log("Updated URL:  " + updated_url);
 		assert.ok(updated_url.indexOf(original_url) === 0, "Updated URL begins with original base URL.");
 		assert.ok(updated_url.indexOf("AWSAccessKeyId") > -1, "Updated URL contains AWSAccessKeyId.");
 		assert.ok(updated_url.indexOf("Expires") > -1, "Updated URL contains Expires.");
 		assert.ok(updated_url.indexOf("Signature") > -1, "Updated URL contains Signature.");
+		check_signed_URL();
 		done();
 	}
 
+	// Give get_private_S3_resource 2 seconds to run
 	setTimeout(check_urls, 2000);
 });
 
+function check_signed_URL() {
+	QUnit.test("Signed URL returns HTTP status 200", function(assert) {
+		var signed_url = document.getElementById("test-preview").src;
+		var done = assert.async();
+
+		var xhr = new XMLHttpRequest();
+		xhr.open("GET", signed_url);
+		xhr.onreadystatechange = function() {
+			if (xhr.readyState === 4) {
+				assert.ok(xhr.status == 200, "Signed URL returns 200 Http response.");
+				done();
+			}
+		};
+		xhr.send();
+	});
+}
