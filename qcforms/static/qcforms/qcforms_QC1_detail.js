02257e67f41285d7c58781b840b5a1bef01af602
Scott Edwards <scott.edwards3@gmail.com>
2016-04-09 21:35:04 -0400

    tweak QC1_detail onload function

--- qcforms/static/qcforms/qcforms_QC1_detail.js
+++ qcforms/static/qcforms/qcforms_QC1_detail.js
@@ -1,7 +1,9 @@
 // GET private image resource for thumbnails
 (function () {
-	var previews = document.body.onload.getElementsByClassName("image-previews");
-	for (var i = previews.length - 1; i >= 0; i--) {
-		get_s3_resource(previews[i], amz_sign_s3);
+	document.body.onload = function () {
+		var previews = document.getElementsByClassName("image-previews");
+		for (var i = previews.length - 1; i >= 0; i--) {
+			get_s3_resource(previews[i], amz_sign_s3);
+		}
 	}
 })();
