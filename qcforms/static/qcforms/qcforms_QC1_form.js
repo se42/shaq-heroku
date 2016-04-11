c95641c83863651d03ea0a2bb2d743c43946eadd
Scott Edwards <scott.edwards3@gmail.com>
2016-04-11 16:59:59 -0400

    first stab at updating the URL field

--- qcforms/static/qcforms/qcforms_QC1_form.js
+++ qcforms/static/qcforms/qcforms_QC1_form.js
@@ -1,19 +1,21 @@
 $(document).ready(function() {
-	$(".image-previews").each(function() {
+	$(".image-preview").each(function() {
 		get_s3_resource(this, amz_sign_s3);
 	});
 });
 
 
 $(".image-col").each(function() {
-	$(this).children(".image-inputs").change(function() {
+	var previewElement = $(this).children(".image-preview")[0];
+	var imageUrlInput = $(this).children(".form-group").children("input")[0];
+	$(this).children(".image-input").change(function() {
 		var file = this.files[0];
 		var folder = "int-nc-form";
-		var previewElement = $(this).next()[0];
 		if (file != null) {
 			upload_private_S3_resource(file, folder, function(url) {
 				previewElement.src = url;
 				get_s3_resource(previewElement, amz_sign_s3);
+				imageUrlInput.value = url;
 			});
 		}
 	});
