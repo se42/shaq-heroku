6cd5e698939bb9b5dd7dad71c3d562711ad1d949
Scott Edwards <scott.edwards3@gmail.com>
2016-04-11 19:05:53 -0400

    trying named setImageColOnChange function

--- qcforms/static/qcforms/qcforms_QC1_form.js
+++ qcforms/static/qcforms/qcforms_QC1_form.js
@@ -2,10 +2,29 @@ $(document).ready(function() {
 	$(".image-preview").each(function() {
 		get_s3_resource(this, amz_sign_s3);
 	});
+	setImageColOnChange();
 });
 
 
-$(".image-col").each(function() {
+// $(".image-col").each(function() {
+// 	var previewElement = $(this).children(".image-preview")[0];
+// 	var imageUrlInput = $(this).children(".form-group").children("input")[0];
+// 	$(this).children(".image-input").change(function() {
+// 		var file = this.files[0];
+// 		var folder = "int-nc-form";
+// 		if (file != null) {
+// 			upload_private_S3_resource(file, folder, function(url) {
+// 				previewElement.src = url;
+// 				get_s3_resource(previewElement, amz_sign_s3);
+// 				imageUrlInput.value = url;
+// 			});
+// 		}
+// 	});
+// });
+
+
+function setImageColOnChange() {
+	$(".image-col").each(function() {
 	var previewElement = $(this).children(".image-preview")[0];
 	var imageUrlInput = $(this).children(".form-group").children("input")[0];
 	$(this).children(".image-input").change(function() {
@@ -20,6 +39,8 @@ $(".image-col").each(function() {
 		}
 	});
 });
+}
+
 
 
 $("#add-image-button").click(function() {
