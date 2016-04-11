3b7a4fe87d7e06b35f9cf05f8afe27c904a2ac66
Scott Edwards <scott.edwards3@gmail.com>
2016-04-11 14:47:00 -0400

    change detail onload to jQuery ready function

--- qcforms/static/qcforms/qcforms_QC1_detail.js
+++ qcforms/static/qcforms/qcforms_QC1_detail.js
@@ -1,9 +1,15 @@
 // GET private image resource for thumbnails
-(function () {
-	document.body.onload = function () {
-		var previews = document.getElementsByClassName("image-previews");
-		for (var i = previews.length - 1; i >= 0; i--) {
-			get_s3_resource(previews[i], amz_sign_s3);
-		}
-	}
-})();
+// (function () {
+// 	document.body.onload = function () {
+// 		var previews = document.getElementsByClassName("image-previews");
+// 		for (var i = previews.length - 1; i >= 0; i--) {
+// 			get_s3_resource(previews[i], amz_sign_s3);
+// 		}
+// 	}
+// })();
+
+$(document).ready(function() {
+	$(".image-previews").each(function() {
+		get_s3_resource(this, amz_sign_s3);
+	});
+});
