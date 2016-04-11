2c4a4d58793c7b296b962d9bbf097b6ad3bee9ea
Scott Edwards <scott.edwards3@gmail.com>
2016-04-11 16:45:54 -0400

    remove commented code

--- qcforms/static/qcforms/qcforms_QC1_form.js
+++ qcforms/static/qcforms/qcforms_QC1_form.js
@@ -4,17 +4,6 @@ $(document).ready(function() {
 	});
 });
 
-// $(".image-inputs").change(function() {
-// 	var file = this.files[0];
-// 	var folder = "int-nc-form";
-// 	var previewElement = $(this).next()[0];
-// 	if (file != null) {
-// 		upload_private_S3_resource(file, folder, function(url) {
-// 			previewElement.src = url;
-// 			get_s3_resource(previewElement, amz_sign_s3);
-// 		});
-// 	}
-// });
 
 $(".image-col").each(function() {
 	$(this).children(".image-inputs").change(function() {
