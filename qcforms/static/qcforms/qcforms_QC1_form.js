46d805035acc86dc2673e6fcebd51085c38f1173
Scott Edwards <scott.edwards3@gmail.com>
2016-04-11 20:10:17 -0400

    first attempt at complete add-image-button implementation

--- qcforms/static/qcforms/qcforms_QC1_form.js
+++ qcforms/static/qcforms/qcforms_QC1_form.js
@@ -6,23 +6,6 @@ $(document).ready(function() {
 });
 
 
-// $(".image-col").each(function() {
-// 	var previewElement = $(this).children(".image-preview")[0];
-// 	var imageUrlInput = $(this).children(".form-group").children("input")[0];
-// 	$(this).children(".image-input").change(function() {
-// 		var file = this.files[0];
-// 		var folder = "int-nc-form";
-// 		if (file != null) {
-// 			upload_private_S3_resource(file, folder, function(url) {
-// 				previewElement.src = url;
-// 				get_s3_resource(previewElement, amz_sign_s3);
-// 				imageUrlInput.value = url;
-// 			});
-// 		}
-// 	});
-// });
-
-
 function setImageColOnChange() {
 	$(".image-col").each(function() {
 	var previewElement = $(this).children(".image-preview")[0];
@@ -42,27 +25,36 @@ function setImageColOnChange() {
 }
 
 
-
 $("#add-image-button").click(function() {
 	var totalForms = document.getElementById("id_intncimage_set-TOTAL_FORMS");
-	var x = totalForms.value;
+	var nextSetID = totalForms.value;
 	totalForms.value++;
 	$("#image-panel").append("<div class=\"image-col col-xs-12 col-sm-6 col-md-4\">"
 				+ "<input class=\"image-input\" type=\"file\"/>"
 				+ "<img class=\"image-preview img-responsive img-thumbnail\" "
-				+ "src=\"http://placehold.it/200x200\"></div>");
+				+ "src=\"https://placehold.it/200x200\">"
+				+ "<div class=\"form-group\">"
+				+ "<label class=\"control-label\" for=\"id_intncimage_set-"
+				+ nextSetID+"-image_url\">Image url</label>"
+				+ "<input class=\"form-control\" id=\"id_intncimage_set-"
+				+ nextSetID+"-image_url\" maxlength=\"200\" name=\"intncimage_set-"
+				+ nextSetID+"-image_url\" placeholder=\"Image url\" "
+				+ "required=\"required\" title=\"\" type=\"url\" />"
+				+ "</div>"
+				+ "<input id=\"id_intncimage_set-"+nextSetID+"-id\" "
+				+ "name=\"intncimage_set-"+nextSetID+"-id\" type=\"hidden\" />"
+				+ "<input id=\"id_intncimage_set-"+nextSetID+"-report\" "
+				+ "name=\"intncimage_set-"+nextSetID+"-report\" type=\"hidden\" />"
+				+ "</div>");
+	if (reportID > 0) {
+		document.getElementById("id_intncimage_set-"+nextSetID+"-report").value = reportID;
+	}
 	setImageColOnChange();
-	/*
-	2 - add new image-col div
-	3 - add new image-input file input
-	4 - add new image-preview img element
-	5 - create new form-group div with label/input elements for url field
-	6 - create new hidden input for id_intncimage_set-x-id with no value
-		--> you should be able to determine the next x from the management_form data
-	7 - create new hidden input for id_intncimage_set-x-report with
-		value = {{ image_form.report.id }} or whatever it is
-	8 - increment management_form TOTAL_FORMS count value
-	9 - disable this button if there is an empty image input/url field available
-	*/
+	$("#add-image-button").attr("disabled", "disabled");
+	var openURLFieldID = "id_intncimage_set-"+nextSetID+"-image_url";
+	var openURLField = document.getElementById(openURLFieldID);
+	openURLField.onchange = function() {
+		$("#add-image-button").attr("disabled", null);
+	};
 });
 
